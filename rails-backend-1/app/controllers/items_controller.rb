2e130cae1ef116c622c3e1fcc93e6abc68ea6bd1
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-07 10:33:36 -0500

    working only using props/setState

---
+++ rails-backend-1/app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: ItemSerializer.new(items)
+    end
+
+    def show
+        item = Item.find_by_id(params[:id])
+        render json: ItemSerializer.new(item)
+    end
+
+    def destroy
+        Item.find_by_id(params[:id]).destroy
+        items = Item.all
+        render json: {
+            items: items
+        }
+    end
+
+end
