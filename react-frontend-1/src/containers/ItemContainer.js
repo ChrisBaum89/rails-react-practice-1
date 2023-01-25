807e15169945b74928e62bd66273c3460724da30
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-25 07:03:45 -0500

    file created and structure setup

---
+++ react-frontend-1/src/containers/ItemContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ItemContainer() {
+
+    return(
+        <div>
+            {/* input form */}
+            {/* list of items */}
+        </div>
+    )
+}
+
+export default ItemContainer
