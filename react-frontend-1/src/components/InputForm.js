8eb11bfd6d1e87ece5ab11aed6edf76ab916b99a
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-25 07:31:51 -0500

    created form and added controlled input

--- react-frontend-1/src/components/InputForm.js
+++ react-frontend-1/src/components/InputForm.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputForm(){
+function InputForm(props){
+    const [formInput, setFormInput] = useState('')
+
+    // controlled form input
+    const handleOnChange = (event) => {
+        setFormInput(event.target.value)
+    }
+
     return(
         <div>
-            import form
+            <form>
+                <input onChange={handleOnChange}></input>
+                <button onSubmit={props.onSubmit}>Submit</button>
+            </form>
         </div>
     )
 }
